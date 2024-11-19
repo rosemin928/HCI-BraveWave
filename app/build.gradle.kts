38cc18483e632ec69b329e73cc9a940bb4b953d5
lucy1287 <lucy1287@naver.com>
2024-11-19 13:53:52 +0900

    2024-11-19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
 }
--- app/src/main/java/com/example/hci/HomeFragment.kt
+++ app/src/main/java/com/example/hci/HomeFragment.kt
@@ -1,12 +1,26 @@
 package com.example.hci
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.example.hci.databinding.ActivityMainBinding
+import com.example.hci.databinding.FragmentHomeBinding
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import java.io.IOException
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +31,34 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val button1 = binding.button1
+        Log.d("button111", button1.text.toString())
+        button1.setOnClickListener {
+            Log.d("button1", button1.toString())
+            sendPostRequest()
+        }
+        return binding.root
+    }
+
+    private fun sendPostRequest() {
+        Log.d("sss", "sss")
+        val url = "http://172.20.19.128:5000/run-muse"
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url(url)
+            .post(RequestBody.create(null, ""))
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                println("Response: ${response.body?.string()}")
+            }
+        })
     }
 }
